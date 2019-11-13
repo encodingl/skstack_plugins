1982b33349c8e32f94d4c01275b99c440663d0d1
encodingl <encodingl@sina.com>
2019-11-13 10:09:15 +0800

    fix01

---
+++ pl_deploy_git/pre_download_source.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from optparse import OptionParser
+import sys
+import os
+import re
+
+# Part2:load skstack_plugins root path for load lib_pub
+CONFIG_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+# Part3:load skstack lib_pub module 
+from lib_pub.common import load_pri_json_conf
+from .lib_pri.git import git_check_out_by_commit_num
+
+
+
+def parseOption(argv):
+    parser = OptionParser(version="%prog 1.0.0")
+
+    parser.add_option("-p", "--git-proj", dest="proj", metavar="[git_project_name]",
+                        help="the static git project name you want to depoly")
+
+    parser.add_option("-e", "--environment", dest="env", metavar="[prod|stg|dev]",
+                      help="the environment you need deploy ")
+
+    parser.add_option("-i", "--commit-id", dest="id", metavar="[git_commit_id]",default="master",
+                      help="the git commit id you want to depoly")
+
+    parser.add_option("-f", "--file-name", dest="file", metavar="[file_name]",
+                      help="the file name you want to depoly")
+
+    args = parser.parse_args()
+    if not len(argv): parser.print_help();sys.exit(1) 
+    return args
+
+
+def main(argv):
+    options = parseOption(argv)
+    env = options.env
+    hosts = options.proj
+    proj = hosts
+
+    proj_type = load_pri_json_conf(CONFIG_BASE_DIR,env, proj)["type"]
+    repo_url = load_pri_json_conf(CONFIG_BASE_DIR,env, proj)["repo_url"]
+    proj_local_path = load_pri_json_conf(CONFIG_BASE_DIR,env, proj)["proj_local_path"]
+
+    if proj_type == "git_nodejs":
+        commit_str = options.id
+        pattern = re.compile(r'\w+')
+        m = pattern.match(commit_str)
+        commit_id = m.group(0)
+        git_check_out_by_commit_num(repo_url, proj_local_path, commit_id)
+    else:
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
